d6e82f5990cc9589a307f3227ee9599c29e85c1c
Bill Tihen <btihen@gmail.com>
2018-03-29 11:14:18 +0200

    just using citext for text insenstive searches

--- db/migrate/20180327155600_add_extensions.rb
+++ db/migrate/20180327155600_add_extensions.rb
@@ -1,5 +1,6 @@
 class AddExtensions < ActiveRecord::Migration[5.2]
-  # def change
-  #   enable_extension :postgis
-  # end
+  def change
+    enable_extension :citext
+    # enable_extension :postgis
+  end
 end
