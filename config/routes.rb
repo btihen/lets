e241e5e262ca3a94d743b2a5d8c45057a995bbe1
Bill Tihen <btihen@gmail.com>
2018-03-30 21:33:48 +0200

    add a static home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home', to: "static_pages#home"
+  get 'static_pages/home'
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tree_measurements
   resources :tree_species
   resources :tree_plots
 
-  root to: "tree_measurements#index"
+  root to: "static_pages#home"
 end
