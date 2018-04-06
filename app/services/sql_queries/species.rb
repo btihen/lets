97ba973b62d8ece18ef0910d94dc3eef5c9e09c4
Bill Tihen <btihen@gmail.com>
2018-04-06 05:49:41 +0200

    clean-up / refactor code for readability

--- app/services/sql_queries/species.rb
+++ app/services/sql_queries/species.rb
@@ -2,15 +2,8 @@ module SqlQueries
 
   class Species
 
-    def initialize(request)
-      case request
-      when :plot_count_by_date
-        @sql_query = species_per_plot_count_per_date
-      when :yearly_avg_by_elevation
-        @sql_query = avg_species_per_elevation_per_year
-      else
-        raise NotImplementedError
-      end
+    def initialize(data_request)
+      @sql_query = choose_sql_query(data_request)
     end
 
     def run
@@ -23,21 +16,25 @@ module SqlQueries
 
     protected
 
-    attr_reader :sql_query
+      attr_reader :sql_query
 
-    def perform
-      ActiveRecord::Base.connection.execute(sql_query)
-    end
+      def perform
+        ActiveRecord::Base.connection.execute(sql_query)
+      end
 
     private
 
-    # sql resources:
-    # http://www.dofactory.com/sql/select-distinct
-    # https://stackoverflow.com/questions/46411130/rails-activerecord-raw-sql-read-data-without-loading-everything-into-memory-wit?rq=1
-    # https://stackoverflow.com/questions/22715130/mysql-nested-query-and-group-by
-    # https://stackoverflow.com/questions/36320861/convert-decimal-number-to-int-sql
-    # https://stackoverflow.com/questions/42983935/summarize-an-array-of-hashes-with-a-crosstab-also-called-a-pivot-table
-    # https://stackoverflow.com/questions/17492167/group-query-results-by-month-and-year-in-postgresql
+    def choose_sql_query(data_request)
+      case data_request
+      when :plot_count_by_date
+        sql_query = species_per_plot_count_per_date
+      when :yearly_avg_by_elevation
+        sql_query = avg_species_per_elevation_per_year
+      else
+        raise NotImplementedError
+      end
+      sql_query
+    end
 
     def species_per_plot_count_per_date
       %{
@@ -52,44 +49,12 @@ module SqlQueries
             ON tree_measurements.tree_specy_id = tree_species.id
         GROUP BY tree_plots.plot_code, tree_plots.elevation_m,
           tree_measurements.measurement_date, tree_species.species_code
-        ORDER BY tree_measurements.measurement_date DESC,
+        ORDER BY extract(year from tree_measurements.measurement_date) DESC,
+          tree_measurements.measurement_date DESC,
           tree_plots.elevation_m, tree_plots.plot_code, tree_species.species_code
       }
-      # ordered by year (not measurement date)
-      # SELECT tree_measurements.measurement_date,
-      #   tree_plots.elevation_m, tree_plots.plot_code,
-      #   tree_species.species_code,
-      #   COUNT(tree_measurements.tree_specy_id) as species_plot_count
-      # FROM tree_plots
-      #   INNER JOIN tree_measurements
-      #     ON tree_plots.id = tree_measurements.tree_plot_id
-      #   INNER JOIN tree_species
-      #     ON tree_measurements.tree_specy_id = tree_species.id
-      # GROUP BY tree_plots.plot_code, tree_plots.elevation_m,
-      #   tree_measurements.measurement_date, tree_species.species_code
-      # ORDER BY extract(year from tree_measurements.measurement_date) DESC,
-      #   tree_plots.elevation_m, tree_plots.plot_code, tree_species.species_code
-      #
-      # ordered by elevation
-      # SELECT tree_plots.plot_code, tree_plots.elevation_m,
-      #   tree_measurements.measurement_date,
-      #   tree_species.species_code,
-      #   SUM(tree_measurements.tree_label) as species_plot_count
-      # FROM tree_plots
-      #   INNER JOIN tree_measurements
-      #     ON tree_plots.id = tree_measurements.tree_plot_id
-      #   INNER JOIN tree_species
-      #     ON tree_measurements.tree_specy_id = tree_species.id
-      # GROUP BY tree_plots.plot_code, tree_plots.elevation_m,
-      #   tree_measurements.measurement_date, tree_species.species_code
-      # ORDER BY tree_plots.elevation_m, tree_measurements.measurement_date,
-      #   tree_species.species_code
     end
 
-    # average grouped by elevation & time frame
-    # (input of first sql into second sum)
-    # https://stackoverflow.com/questions/22715130/mysql-nested-query-and-group-by
-    # https://stackoverflow.com/questions/36320861/convert-decimal-number-to-int-sql
     def avg_species_per_elevataion_per_date
       %{
         SELECT elevation_m, measurement_date, species_code,
@@ -117,18 +82,20 @@ module SqlQueries
           ORDER BY extract(year from temp.measurement_date) DESC,
             temp.elevation_m, temp.species_code
       }
-      # SELECT
-      #     CAST(extract(year from measurement_date) AS INTEGER) AS year,
-      #     elevation_m, species_code,
-      #     CAST(AVG(species_plot_count) AS INTEGER) AS avg_species_count
-      #   FROM (
-      #     #{species_per_plot_count_per_date}
-      #   ) as temp
-      #   GROUP BY extract(year from temp.measurement_date),
-      #     temp.elevation_m, temp.species_code
-      #   ORDER BY extract(year from temp.measurement_date),
-      #     temp.elevation_m, temp.species_code
     end
+
+    # sql resources:
+    # http://www.dofactory.com/sql/select-distinct
+    # https://stackoverflow.com/questions/46411130/rails-activerecord-raw-sql-read-data-without-loading-everything-into-memory-wit?rq=1
+    # https://stackoverflow.com/questions/22715130/mysql-nested-query-and-group-by
+    # https://stackoverflow.com/questions/36320861/convert-decimal-number-to-int-sql
+    # https://stackoverflow.com/questions/42983935/summarize-an-array-of-hashes-with-a-crosstab-also-called-a-pivot-table
+    # https://stackoverflow.com/questions/17492167/group-query-results-by-month-and-year-in-postgresql
+
+    # average grouped by elevation & time frame
+    # (input of first sql into second sum)
+    # https://stackoverflow.com/questions/22715130/mysql-nested-query-and-group-by
+    # https://stackoverflow.com/questions/36320861/convert-decimal-number-to-int-sql
 
     # def crosstab
     #   # CROSSTAB Data - seems too complex for my SQL
